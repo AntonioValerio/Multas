e8bf200f508a2bbda6c713e1576516ed68056449
antonio valerio <antoni_valerio@hotmail.com>
2018-04-03 12:51:46 +0100

    WIth working View

--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -15,7 +15,11 @@ namespace Multas.Models
         }
         [Key]
         public int ID { get; set; }
+        [Required(ErrorMessage =" O {0} é de preenchimento obrigatório")]
+        [RegularExpression("[A-ZÀÈÌÒÙÁÉÍÓÚÂÊÎÔÛ][a-záéíóúÀàèìòùâêîôûäëïöüãõç]+ ((|'|-| dos | da | de | e | d'| Mc|)[A-ZÀÈÌÒÙÁÉÍÓÚÂÊÎÔÛ][a-záéíóúÀàèìòùâêîôûäëïöüãõç]+){1,4)", ErrorMessage = "o {0} apenas pode conter letras e espaços em branco.Cada palara começa em Maiúscula ,seguida de minúscula...")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = " A {0} é de preenchimento obrigatório")]
         public string Esquadra { get; set; }
         public string Fotografia { get; set; }
         //lista de multas de 'emite'
